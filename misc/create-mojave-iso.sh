ad9fff51f6fc90c5d325a1465302d1fedc4a7793
Francesco Cagnin <fcagnin@quarkslab.com>
2020-03-22 14:40:54 +0100

    refactor: improve create-mojave-iso.sh

--- misc/create-mojave-iso.sh
+++ misc/create-mojave-iso.sh
@@ -3,10 +3,11 @@ set -e
 dirname () { python -c "import os; print(os.path.dirname(os.path.realpath('$0')))"; }
 cd "$(dirname "$0")"
 
-hdiutil create -o "/tmp/macos-mojave.cdr" -size 6g -layout SPUD -fs HFS+J
+rm -f "/tmp/macos-mojave.cdr.dmg"
+hdiutil create -o "/tmp/macos-mojave.cdr" -size 8g -layout SPUD -fs HFS+J
 
 hdiutil attach "/tmp/macos-mojave.cdr.dmg" -mountpoint "/Volumes/macos-mojave-installmedia"
-sudo "/Applications/Install macOS Mojave.app/Contents/Resources/createinstallmedia" --volume "/Volumes/macos-mojave-installmedia"
+sudo "/Applications/Install macOS Mojave.app/Contents/Resources/createinstallmedia" --volume "/Volumes/macos-mojave-installmedia" --nointeraction
 
 hdiutil detach "/Volumes/Install macOS Mojave"
 hdiutil convert "/tmp/macos-mojave.cdr.dmg" -format UDTO -o "/tmp/macos-mojave.iso"
